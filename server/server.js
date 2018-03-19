0256529d8d6709305ef5aa0df5a209cab9efb184
abequinonez <abequinonez@users.noreply.github.com>
2018-03-19 15:30:20 -0700

    feat: Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -68,6 +68,25 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+// Delete a to-do item
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(err => res.status(400).send());
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
