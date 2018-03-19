64380e12897e11ec6905ae9488c1d665d9af8b4f
abequinonez <abequinonez@users.noreply.github.com>
2018-03-19 01:49:11 -0700

    feat: Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ObjectID } = require('mongodb');
 
 // Import the configured database reference
 const { mongoose } = require('./db/mongoose');
@@ -41,6 +42,29 @@ app.get('/todos', (req, res) => {
       res.status(400).send(err);
     }
   );
+});
+
+// Get the desired to-do item (if it exists)
+app.get('/todos/:id', (req, res) => {
+  // Store the ID in a constant
+  const id = req.params.id;
+
+  // Check if the ID passed in is valid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
