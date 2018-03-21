bd856f08627cbd25811b1921b39a8c0756df58a2
abequinonez <abequinonez@users.noreply.github.com>
2018-03-20 18:47:06 -0700

    refactor: Set up separate development and test environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const dbUrl = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(dbUrl);
