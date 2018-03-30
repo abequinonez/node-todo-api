f9e42a8b093a1370aa1ddc88b89b82e277296cbf
abequinonez <abequinonez@users.noreply.github.com>
2018-03-29 20:05:55 -0700

    test: Add tests for POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -247,3 +247,55 @@ describe('POST /users', () => {
       .end(done);
   });
 });
+
+describe('POST /users/login', () => {
+  it('should log in user and return auth token', done => {
+    request(app)
+      .post('/users/login')
+      .send({ email: usersArr[1].email, password: usersArr[1].password })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(usersArr[1]._id)
+          .then(user => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            });
+            done();
+          })
+          .catch(err => done(err));
+      });
+  });
+
+  it('should reject invalid login', done => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: usersArr[1].email,
+        password: usersArr[1].password + '123'
+      })
+      .expect(400)
+      .expect(res => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(usersArr[1]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(err => done(err));
+      });
+  });
+});
